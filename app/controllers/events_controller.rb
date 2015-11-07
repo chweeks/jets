b8662bccee9c85a13789d381b42cf8911744571f
kyle macpherson <kaimac2010@gmail.com>
2015-11-07 16:06:18 +0000

    changes made to events#create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
-class EventController < ApplicationController
+class EventsController < ApplicationController
 
   def create
     hash = JSON.parse(request.body.read)
     session = Session.find_by_auth_key(hash['auth_key'])
     user = User.find(session.user_id)
+    hash.delete('auth_key')
     event = Event.new(hash, user_id: session.user_id)
     if event.save
-      render json: {"messages": "it worked get in there"}, event
+      render json: event
     else
       render json: {"messages": "FUCKKKKKKKKK"}
     end
