6289eae9e9bbc29305cefe3afcbf8e2f94aa2958
Chris Harry Weeks <chrisharryweeks@gmail.com>
2015-12-02 14:27:25 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :sessions, dependent: :destroy
-  validates_presence_of :email, :password_confirmation
-  validates :email, uniqueness: true
+  validates_presence_of :username, :password_confirmation
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 8 }
   has_many :events
 end
